1ef71c408757fc16b7faa1c8403ebc9f27b61024
mochen <mochen.china@gmail.com>
2020-06-13 15:58:38 +0800

    fix auto yes

--- shell/login.sh
+++ shell/login.sh
@@ -8,7 +8,7 @@ set port [lindex $argv 3]
 
 spawn ssh $user@$host -p $port
 expect {
-  "Are you sure you want to continue connecting (yes/no)?" {
+  "Are you sure you want to continue connecting" {
     send "yes\n"
     expect "password:"
     send "$pwd\n"
